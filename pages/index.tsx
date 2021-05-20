85df4f10e770abe1a2b9810710ff9dd50d548c6a
dadayama <dadayamism@gmail.com>
2021-05-20 22:36:11 +0900

    PR一覧ページの調整

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import Link from 'next/link';
+import Image from 'next/image';
 import { useState, useEffect, useMemo } from 'react';
 import { useTable, Column } from 'react-table';
 import { useOctokit } from '@/hooks/useOctokit';
@@ -11,7 +12,12 @@ type Data = {
   author: string;
   avatarUrl: string;
   pageUrl: string;
+  createdAt: string;
 };
+
+const OWNER = process.env.GITHUB_OWNER;
+const REPOSITORY = `${OWNER}/${process.env.GITHUB_REPOSITORY}`;
+const AUTHOR = process.env.GITHUB_USER;
 
 const TOKEN = process.env.GITHUB_PERSONAL_ACCESS_TOKEN;
 const octokit = useOctokit(TOKEN);
@@ -24,20 +30,28 @@ const Home = () => {
       const {
         data: { items },
       } = await octokit.rest.search.issuesAndPullRequests({
-        q: `org:dayone-jp+author:dadayama+is:pr`,
+        q: `${REPOSITORY}+author:${AUTHOR}+is:pr`,
         sort: 'created',
         order: 'desc',
         per_page: 100,
         page: 1,
       });
       const newData = items.map(
-        ({ number, title, user: { login: author, avatar_url: avatarUrl }, html_url: pageUrl, state }) => ({
+        ({
+          number,
+          title,
+          user: { login: author, avatar_url: avatarUrl },
+          html_url: pageUrl,
+          state,
+          created_at: createdAt,
+        }) => ({
           number,
           author,
           title,
           avatarUrl,
           pageUrl,
           state,
+          createdAt,
         })
       );
 
@@ -55,21 +69,38 @@ const Home = () => {
       {
         Header: 'Author',
         accessor: 'author' as const,
+        Cell: ({ row }) => <Image src={row.values.avatarUrl} alt={row.values.author} width={30} height={30} />,
+      },
+      {
+        Header: 'Avatar URL',
+        accessor: 'avatarUrl' as const,
       },
       {
         Header: 'Title',
         accessor: 'title' as const,
-        Cell: ({ row }) => <Link href={`/pulls/${row.values.number}/reviews`}>{row.values.title}</Link>,
+        Cell: ({ row }) => (
+          <Link href={`/pulls/${row.values.number}/reviews`}>
+            <a>{row.values.title}</a>
+          </Link>
+        ),
       },
       {
         Header: 'State',
         accessor: 'state' as const,
       },
+      {
+        Header: 'Created At',
+        accessor: 'createdAt' as const,
+      },
     ],
     []
   );
 
-  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } = useTable<Data>({ columns, data });
+  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } = useTable<Data>({
+    columns,
+    data,
+    initialState: { hiddenColumns: ['number', 'avatarUrl'] },
+  });
 
   if (data.length) {
     return (
